20947e95c9f028ca86d9901c49b9b1e1fc625646
Masaaki Morishita <ikasama3.0@gmail.com>
2015-04-14 22:57:45 +0900

    write test code

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-var historia = require('./')
+var fs = require('fs')
+var Historia = require('./')
+var historia = new Historia()
+var test = require('tape')
+
+test('setup', function (t) {
+    var actual = historia.setup('sample-history')
+    var expected = "yum install php\nyum install php-mysql\nyum install httpd"
+    t.same(actual, expected)
+    t.end()
+})
